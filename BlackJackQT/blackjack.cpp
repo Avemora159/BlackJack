d8729ff6ca4a1d7b8b5c69a63e2012dfa487aee8
threadx <threadx@THREAD>
2023-03-07 19:30:52 +0200

    Sketch of program Card now struct it's more better

---
+++ BlackJackQT/blackjack.cpp
@@ -0,0 +1,34 @@
+#include "blackjack.h"
+
+
+Hand::Hand(Deck deck) {
+	_money = 20;
+	_count = 0;
+	plus_card(deck);
+	plus_card(deck);
+}
+
+int Hand::get_count()
+{
+	return _count;
+}
+
+void Hand::plus_card(Deck deck) {
+	if (deck.get_card().name == A) {
+		//ask 1 or 11 mb switch
+		_count += 11;
+	}
+	else
+	_count += deck.get_card().name;
+}
+
+
+Deck::Deck() {
+
+}
+
+Card Deck::get_card() {
+	Card back = deck.back();
+	deck.pop_back();
+	return back;
+}
---
+++ BlackJackQT/blackjack.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <iostream>
+#include <vector>
+//player
+class Hand {
+private:
+	int _money;
+	int _count;
+public:
+	Hand(Deck);
+	void plus_card(Deck);
+	int get_count() {
+		return _count;
+	}
+};
+
+enum name { c2 = 2, c3, c4, c5, c6, c7, c8, c9, c10, J = 10, Q = 10, K = 10, A };
+enum c_suit { heart, diamond, spade, club };
+//card
+struct Card {
+	name name;
+	c_suit suit;
+};
+
+
+//deck
+class Deck {
+private:
+	std::vector<Card> deck;
+public:
+	Deck();
+	Card get_card();
+};
+
+//table
+class Table {
+private:
+	std::vector <Hand> players;
+};
